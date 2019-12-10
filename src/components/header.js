a2365d37bc1d159018ff4586b4311d5059f62b3d
Jose Hernando <jose@builtvisible.com>
2019-12-10 13:34:01 +0000

    added react logo

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link, graphql, useStaticQuery } from 'gatsby'
 // import './header.module.scss'
 import headerStyles from './header.module.scss'
+import logo from '../../static/react.png'
 
 const MainHeader = () => {
      const data = useStaticQuery(graphql`
@@ -15,17 +16,21 @@ const MainHeader = () => {
      `)
      return (
           <header className={headerStyles.header}>
+          
                <h1>
                     <Link className={headerStyles.title} to="/">
                     {data.site.siteMetadata.title}
                     </Link>
+                    <img class="logo"></img>
                </h1>
+               
                <nav>
                     <ul className={headerStyles.navList}>
                          <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link></li>
                          <li><Link  className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link></li>
                          <li><Link  className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About me</Link></li>
                          <li><Link  className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">Contact</Link></li>
+                         <li><Link  className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">New</Link></li>
                     </ul>
                </nav>
           </header>
